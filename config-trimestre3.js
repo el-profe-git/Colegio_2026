5b08acb0a7ca57728b0ba6a466f69b1ce324003c
el-profe-git <elp22587@gmail.com>
2026-09-14 09:26:39 -0500

    Add files via upload

--- config-trimestre3.js
+++ config-trimestre3.js
@@ -9,3 +9,19 @@
 _registrarActividad(3, 'Razones y Proporciones', 'matematicas/sexto/trimestre_3/RAZONESYPROPORCIONES/Taller-Razones-Proporciones.html');
 
 _registrarActividad(3, 'Porcentajes', 'matematicas/sexto/trimestre_3/PORCENTAJES/Taller-Porcentajes.html');
+
+_registrarActividad(3, 'Magnitudes', 'matematicas/sexto/trimestre_3/MAGNITUDES/Taller-Magnitudes.html');
+
+_registrarActividad(3, 'PMT2_evaluacion_1', 'matematicas/sexto/trimestre_3/plan_de_mejora_trimestre_2/6_evaluacionpmt2_1.html');
+
+_registrarActividad(3, 'PMT2_evaluacion_2', 'matematicas/sexto/trimestre_3/plan_de_mejora_trimestre_2/6_evaluacionpmt2_2.html');
+
+_registrarActividad(3, 'PMT2_evaluacion_3', 'matematicas/sexto/trimestre_3/plan_de_mejora_trimestre_2/6_evaluacionpmt2_3.html');
+
+_registrarActividad(3, 'PMT2_evaluacion_1', 'fisica/decimo/trimestre_3/plan_de_mejora_trimestre_2/10_evaluacionpmt2_1.html');
+
+_registrarActividad(3, 'PMT2_evaluacion_2', 'fisica/decimo/trimestre_3/plan_de_mejora_trimestre_2/10_evaluacionpmt2_2.html');
+
+_registrarActividad(3, 'PMT2_evaluacion_3', 'fisica/decimo/trimestre_3/plan_de_mejora_trimestre_2/10_evaluacionpmt2_3.html');
+
+
